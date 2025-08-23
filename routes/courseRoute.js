e30a25ef78be481e8e9343d7befcf2de34b4179f
Yasin <yasin.enis.tr@gmail.com>
2025-08-23 22:14:02 +0300

    ✨roles implemented 🔸roles set for database, auth screen 🔸only users who has teacher role can create courses

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -2,9 +2,13 @@ import express from 'express';
 
 import * as courseController from '../controllers/courseController.js';
 
+import roleMiddleware from '../middlewares/roleMiddleware.js';
+
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse); // localhost:3000/courses
+router
+  .route('/')
+  .post(roleMiddleware(['teacher', 'admin']), courseController.createCourse); // localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 
